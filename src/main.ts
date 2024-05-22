b86b24749e6dcf9f55ae279d06340e3bcf730504
Szymon Maj <szymon.maj@futurum.tech>
2024-05-22 15:45:31 +0200

    changed styles and removed unused paramteres

--- src/main.ts
+++ src/main.ts
@@ -9,47 +9,42 @@ import { Project } from './models/project';
 
 const projectApiHelper = new ProjectsApiHelper();
 
-// @ts-ignore: Unused parameter
-const headerContainer = new Container('header-container', 'app');
+new Container('header-container', 'app');
 
-// @ts-ignore: Unused parameter
-const contentContainer = new Container('content-container', 'app');
+new Container('content-container', 'app');
 
 const handleClick = (): void => {
   modal.open();
 };
 
-// @ts-ignore: Unused parameter
-const addButton = new AddButton(
+new AddButton(
   'Add new project',
   'add-project-btn',
   'content-container',
   handleClick
 );
 
-// @ts-ignore: Unused parameter
-const projectsContainer = new Container(
-  'projects-container',
-  'content-container'
-);
+new Container('projects-container', 'content-container');
 
-// @ts-ignore: Unused parameter
 const modal = new Modal('add-form-modal', 'content-container');
 
-// @ts-ignore: Unused parameter
-const headerLabel = new HeaderLabel(
-  'Manage me',
-  'title-header',
-  'header-container'
-);
+new HeaderLabel('Manage me', 'title-header', 'header-container');
 
 const projectForm = new ProjectForm('project-form');
 
+// @ts-ignore: Unused parameter
 projectForm.setOnSubmit((event, project: Project) => {
   projectApiHelper.create(project);
-  // @ts-ignore: Unused parameter
-  const projectItem = new ProjectItem('projects-container', project);
+  new ProjectItem('projects-container', project);
   modal.close();
 });
 
 modal.setContent(projectForm.form);
+
+window.onload = () => {
+  const projects = projectApiHelper.getAll();
+  projects.forEach((project) => {
+    console.log(project);
+    new ProjectItem('projects-container', project);
+  });
+};
